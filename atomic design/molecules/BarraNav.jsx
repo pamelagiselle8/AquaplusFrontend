48f078b049162db614b131838331c88e36969537
Bryan23453 <rayan.andino@gmail.com>
2024-05-22 19:42:41 -0600

    agrego funcionalidad de boton login y ref a las secciones

--- atomic design/molecules/BarraNav.jsx
+++ atomic design/molecules/BarraNav.jsx
@@ -4,9 +4,10 @@ import {
   NavbarBrand,
   NavbarContent,
   NavbarItem,
-  Link,
+  Link ,
   Button,
 } from "@nextui-org/react";
+import {  Link as RouterLink } from "react-router-dom";
 
 export default function BarraNav() {
   return (
@@ -15,33 +16,33 @@ export default function BarraNav() {
         <img id="logo" src="../../assets/LogoTextoHD.png" />
       </NavbarBrand>
       <NavbarContent className=" sm:flex gap-4" justify="center">
-        <NavbarItem>
+        <NavbarItem href="hero-section">
           <Link color="foreground" href="#">
             Sobre nosotros
           </Link>
         </NavbarItem>
-        <NavbarItem isActive>
+        <NavbarItem href="mision">
           <Link href="#" aria-current="page">
             Misión y visión
           </Link>
         </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="#">
+        <NavbarItem href="contactanos"> 
+          <Link color="foreground">
             Contáctanos
           </Link>
         </NavbarItem>
       </NavbarContent>
       <NavbarContent justify="end">
         <NavbarItem>
-          <Button
-            className="rounded-large shadow-small"
-            as={Link}
-            color="primary"
-            href="#"
-            variant="flat"
-          >
-            Iniciar sesión
-          </Button>
+        <RouterLink to="/login">
+            <Button
+              className="rounded-large shadow-small"
+              color="primary"
+              variant="flat"
+            >
+              Iniciar sesión
+            </Button>
+          </RouterLink>
         </NavbarItem>
       </NavbarContent>
     </Navbar>
--- atomic design/templates/Inicio.tsx
+++ atomic design/templates/Inicio.tsx
@@ -14,7 +14,14 @@ function Inicio() {
           </div>
         </div>
       </section>
-      <section id="contacto-section">
+
+
+      <section id="mision">
+        <img src="https://images.unsplash.com/photo-1615529328331-f8917597711f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+      </section>
+
+
+      <section id="contactanos">
         <img src="https://images.unsplash.com/photo-1615529328331-f8917597711f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
       </section>
     </>
