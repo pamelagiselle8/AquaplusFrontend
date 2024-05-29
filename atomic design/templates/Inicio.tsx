36bd5ca97aec8a2d25c60fcf7d5b4887dbd54c47
pamelagiselle8 <pamelaramirez@unitec.edu>
2024-05-29 00:32:14 -0600

    Commit para merge con main

--- atomic design/templates/Inicio.tsx
+++ atomic design/templates/Inicio.tsx
@@ -14,9 +14,9 @@ function Inicio() {
           </div>
         </div>
       </section>
-      <section id="contacto-section">
+      {/* <section id="contacto-section">
         <img src="https://images.unsplash.com/photo-1615529328331-f8917597711f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-      </section>
+      </section> */}
     </>
   );
 }
