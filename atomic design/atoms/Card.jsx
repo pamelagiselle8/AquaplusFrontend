17a3de23813f6c3965d04206423ba3f3c0745988
Daniel <danielisaacjuarez@gmail.com>
2024-06-05 17:41:40 -0600

    update

--- atomic design/atoms/Card.jsx
+++ atomic design/atoms/Card.jsx
@@ -1,26 +1,13 @@
-// import React from "react";
-// import { Card, CardBody } from "@nextui-org/react";
-
-// export default function App({ text }) {
-//     return (
-//         <Card>
-//             <CardBody>
-//                 <p>{text}</p>
-//             </CardBody>
-//         </Card>
-//     );
-// }
-
 import React from "react";
-import { Card, CardHeader, CardBody, CardFooter, Divider, Link, Image } from "@nextui-org/react";
+import { Card, CardHeader, CardBody } from "@nextui-org/react";
 
 export default function App({ title, text }) {
     return (
-        <Card className="max-w-[400px]">
-            <CardHeader className="flex gap-3">
-                <p className="text-md">{title}</p>
+        <Card>
+            <CardHeader style={{ textAlign: 'center', display: 'flex', justifyContent: 'center' }}>
+                <p>{title}</p>
             </CardHeader>
-            <CardBody>
+            <CardBody style={{ textAlign: 'center', justifyContent: 'center' }}>
                 <p>{text}</p>
             </CardBody>
         </Card>
--- atomic design/templates/Inicio.tsx
+++ atomic design/templates/Inicio.tsx
@@ -17,21 +17,15 @@ function Inicio() {
       </section>
 
       <section id="Mision&Vision" style={{ position: 'relative', textAlign: 'center' }}>
-        <img
-          src="https://images.unsplash.com/photo-1615529328331-f8917597711f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        <img 
+          src="https://images.unsplash.com/photo-1615529328331-f8917597711f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
           style={{ width: '100%', height: 'auto' }}
         />
         <div
           style={{
-            position: 'absolute',
-            top: '70%',
-            left: '0%',
-            // transform: 'translate(-50%, -50%)',
+            position: 'absolute', top: '70%', left: '2%', right: '2%',
             backgroundColor: 'rgba(255, 255, 255, 0.4)',
-            padding: '20px',
-            borderRadius: '25px',
-            display: 'flex',
-            gap: '20px'
+            padding: '20px', borderRadius: '25px', display: 'flex', gap: '20px'
           }}
         >
           <Card title="Misión" text="Lorem ipsum dolor sit amet, eam natum animal in, eos nominavi lucilius no. Ex case possit audire quo. Rebum movet audiam in ius, ea case nemore praesent per, sea congue saperet ad. At pri discere atomorum philosophia. Has ignota integre ea. Has no commodo omittantur, te tantas partem quaestio vix. Sea id putent appareat senserit." />
