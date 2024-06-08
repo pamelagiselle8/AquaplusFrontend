9de4e226e84f7aea64498f1d9fc8b4beaa602f03
Daniel <danielisaacjuarez@gmail.com>
2024-06-07 21:32:02 -0600

    mediaCards push

--- atomic design/templates/Inicio.tsx
+++ atomic design/templates/Inicio.tsx
@@ -1,5 +1,6 @@
 import BarraNav from "../molecules/BarraNav";
 import Card from "../atoms/Card";
+import MediaCard from "../atoms/MediaCard";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
@@ -57,8 +58,41 @@ function Inicio() {
       </section>
 
       <section id="Contactanos">
-        <img src="https://images.unsplash.com/photo-1615529328331-f8917597711f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-      </section>
+    <img src="https://images.unsplash.com/photo-1615529328331-f8917597711f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Background" />
+    <div
+        style={{
+            position: 'absolute', top: '175%', left: '2%', right: '2%',
+            backgroundColor: 'rgba(255, 255, 255, 0.4)', justifyContent: 'center',
+            padding: '20px', borderRadius: '25px', display: 'flex', gap: '30px'
+        }}
+    >
+        <MediaCard 
+            ImageSource="assets/instagramlogo.png" 
+            Title="Nuestro Instagram" 
+            User="@AquaplusHN" 
+            Text="Slogan para Instagra," 
+            HyperlinkText="Ir a Instagram" 
+            MediaLink="https://github.com/nextui-org/nextui" 
+        />
+        <MediaCard 
+            ImageSource="assets/facebooklogo.png" 
+            Title="Nuestro Facebook" 
+            User="Aquaplus" 
+            Text="Slogan para Facebook" 
+            HyperlinkText="Ir a Facebook" 
+            MediaLink="https://github.com/nextui-org/nextui" 
+        />
+        {/* <MediaCard 
+            ImageSource="assets/whatsapplogo.png" 
+            Title="Nuestro WhatsApp" 
+            User="+504 9999-9999" 
+            Text="Slogan para WhatsApp" 
+            HyperlinkText="Ir a WhatsApp" 
+            MediaLink="https://github.com/nextui-org/nextui" 
+        /> */}
+    </div>
+</section>
+
     </>
   );
 }
