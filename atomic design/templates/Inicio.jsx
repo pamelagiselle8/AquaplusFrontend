9c7a34f713e8cbdceb5e38067817b31ff7b0a52a
pamelagiselle8 <pamelaramirez@unitec.edu>
2024-06-10 19:45:45 -0600

    Commit para que daniel siga trabajando

--- atomic design/templates/Inicio.jsx
+++ atomic design/templates/Inicio.jsx
@@ -128,7 +128,7 @@ function Inicio({ modoEditar = false }) {
       </section>
 
       <section id="Contactanos">
-        {/* <img src="https://images.unsplash.com/photo-1615529328331-f8917597711f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" /> */}
+        <img src="https://images.unsplash.com/photo-1615529328331-f8917597711f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
         <ContactanosSection />
       </section>
     </>
