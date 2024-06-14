139f4d381b041aa942e1885f8e916f2dbc52e968
pamelagiselle8 <pamelaramirez@unitec.edu>
2024-06-14 15:35:01 -0600

    Arreglada segunda imagen mision-vision

--- atomic design/molecules/VisionMision.jsx
+++ atomic design/molecules/VisionMision.jsx
@@ -3,15 +3,15 @@ import { Image } from "@nextui-org/react";
 export default function VisionMision({ titulo, contenido, icono, imagen }) {
   return (
     <div className="width-window ">
-      <div className="icono-texto">
+      <div className="div-icono-texto">
         <img className="icono" src={icono} />
-        <div className="texto">
+        <div className="div-texto">
           <h1 className="text-xl text-primary titulo">{titulo}</h1>
           <h1 className="text-lg font-light text-default">{contenido}</h1>
         </div>
       </div>
-      <div className="imagen">
-        <Image className="" src={imagen} />
+      <div className="div-imagen">
+        <Image width={500} shadow="sm" src={imagen} />
       </div>
     </div>
   );
