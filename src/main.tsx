47a5ede42f8c585f6f40402f401d446361a72864
pamelagiselle8 <pamelaramirez@unitec.edu>
2024-06-16 01:53:36 -0600

    aqui vamos de nuevo

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Inicio from "../atomic design/templates/Inicio.jsx";
-import Login from "../atomic design/templates/Login.jsx";
+import Inicio from "./atomic design/templates/Inicio.jsx";
+import Login from "./atomic design/templates/Login.jsx";
 import "./index.css";
 import { NextUIProvider } from "@nextui-org/react";
 
